8dcfcaf6c96f50ad9844ce29873cabf846b77bfa
Alexandre Estevao <alexandresestevao@gmail.com>
2022-02-23 16:46:09 -0300

    Obtendo post por Id

---
+++ src/main/java/com/alexandreestevao/springmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.alexandreestevao.springmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexandreestevao.springmongodb.domain.Post;
+import com.alexandreestevao.springmongodb.repositories.PostRepository;
+import com.alexandreestevao.springmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado!"));
+	}
+
+}
