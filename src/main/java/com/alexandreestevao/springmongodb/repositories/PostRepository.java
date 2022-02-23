cd6039961571babc60c823fec8c7aec2285ea137
Alexandre Estevao <alexandresestevao@gmail.com>
2022-02-23 16:00:05 -0300

    Criando entity Post com User alinhado

--- src/main/java/com/alexandreestevao/springmongodb/repositories/PostRepository.java
+++ src/main/java/com/alexandreestevao/springmongodb/repositories/PostRepository.java
@@ -3,9 +3,9 @@ package com.alexandreestevao.springmongodb.repositories;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.alexandreestevao.springmongodb.domain.User;
+import com.alexandreestevao.springmongodb.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String>{	
-	
+public interface PostRepository extends MongoRepository<Post, String>{
+
 }
